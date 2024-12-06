f2eb967fb7020d88ae602a17e07c62c781ac730e
郭佳宝 <161409226+flySkyBigHold@users.noreply.github.com>
2024-12-06 16:19:01 +0800

    Delete mybatis-generator directory

--- mybatis-generator/src/main/java/cn/system/scf/cms/md/dao/SystemFunctionMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.system.scf.cms.md.dao;
-
-import cn.system.scf.cms.md.model.SystemFunction;
-import cn.system.scf.cms.util.annotation.MyMapper;
-
-public interface SystemFunctionMapper extends MyMapper<SystemFunction> {
-}
--- mybatis-generator/src/main/java/cn/system/scf/cms/md/dao/SystemModelMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.system.scf.cms.md.dao;
-
-import cn.system.scf.cms.md.model.SystemModel;
-import cn.system.scf.cms.util.annotation.MyMapper;
-
-public interface SystemModelMapper extends MyMapper<SystemModel> {
-}
